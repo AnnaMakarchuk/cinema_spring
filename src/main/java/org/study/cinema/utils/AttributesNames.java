cf225227c651ce70fefb8f51fa1fd24d952b9f1f
Anna Makarchuk <anna_makarchuk@epam.com>
2019-08-20 15:43:59 +0300

    update bundle file and parameters names

--- src/main/java/org/study/cinema/utils/AttributesNames.java
+++ src/main/java/org/study/cinema/utils/AttributesNames.java
@@ -1,6 +1,15 @@
 package org.study.cinema.utils;
 
 public class AttributesNames {
+    public static final String CLIENTS = "clients";
+    public static final String USER = "user";
+    public static final String ADMIN = "admin";
+    public static final String NAME = "name";
+    public static final String SURNAME = "surname";
+    public static final String GENDER = "gender";
+    public static final String LOGIN = "login";
+    public static final String EMAIL = "email";
+    public static final String PASSWORD = "password";
 
     public static final String MOVIES = "movies";
     public static final String MOVIE = "movie";
